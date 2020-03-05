a1dcd869f2d6ae886991a8fa296fb4d1601448fb
Reinaldo Borges Salla <reinaldo.salla@pampa.compasso>
2020-03-05 08:21:19 -0300

    Change database name to 'tarefa5'

--- server/src/properties.ts
+++ server/src/properties.ts
@@ -5,6 +5,6 @@ export const hostname: string = "localhost";
 export const route: string = `${hostname}/${port}`
 export const routeApi: string = `${route}/api`
 export const negotiationsRoute: string = "/negotiations";
-export const dbName: string = "nestjs-demo";
+export const dbName: string = "tarefa5";
 export const dbUrl: string = `mongodb://${hostname}/${dbName}`;
 export const clientDir: string = join(__dirname, "..", "..", "client");
