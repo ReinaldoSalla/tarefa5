d0b67619a5e7d94fcc4aeb7d10652772bb3b0918
Reinaldo Borges Salla <reinaldo.salla@pampa.compasso>
2020-04-08 08:50:24 -0300

    Added security

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,12 +5,25 @@ import { port } from "./properties";
 import { route } from "./properties";
 import { clientDir } from "./properties";
 import { dbUrl } from "./properties";
+import * as helmet from "helmet";
+import * as csurf from "csurf";
+import * as rateLimit from "express-rate-limit";
+
+const env = process.env.NODE_ENV === "production" ? "production" : "development";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
   app.enableCors();
+  //app.use(csurf()); // if using cookies and autorization/authentication
+  if (env === "production") {
+    app.use(rateLimit({
+      windowMs: 60 * 1000, // 1 minute
+      max: 100, // limit each IP to 100 requests/minute
+    }));
+  }
   await app.listen(port);
   console.log(`Server running. URL: http://${route}`);
   console.log(`MongoDB connected. URL: ${dbUrl}`);
